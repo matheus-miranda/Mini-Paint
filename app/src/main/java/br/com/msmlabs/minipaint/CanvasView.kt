e5e17184b95f403854d6d1d9cd9663f8311ae069
matheus-miranda <matheus.sm86@gmail.com>
2021-09-24 23:30:14 -0300

    Place bitmap in the canvas

--- app/src/main/java/br/com/msmlabs/minipaint/CanvasView.kt
+++ app/src/main/java/br/com/msmlabs/minipaint/CanvasView.kt
@@ -1,9 +1,32 @@
 package br.com.msmlabs.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class CanvasView(context: Context) : View(context) {
 
+    // Used for caching what has been drawn before
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
 
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        // Recycle Bitmap before creating a new one to prevent memory leak
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        // Bitmap with the screen size
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
 }
