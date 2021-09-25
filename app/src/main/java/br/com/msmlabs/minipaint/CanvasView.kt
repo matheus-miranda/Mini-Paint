2d7d8eacc06132e2945552c0475428dcaf3c4030
matheus-miranda <matheus.sm86@gmail.com>
2021-09-24 23:18:43 -0300

    Set the canvas on the screen

---
+++ app/src/main/java/br/com/msmlabs/minipaint/CanvasView.kt
@@ -0,0 +1,9 @@
+package br.com.msmlabs.minipaint
+
+import android.content.Context
+import android.view.View
+
+class CanvasView(context: Context) : View(context) {
+
+
+}
--- app/src/main/java/br/com/msmlabs/minipaint/MainActivity.kt
+++ app/src/main/java/br/com/msmlabs/minipaint/MainActivity.kt
@@ -2,10 +2,15 @@ package br.com.msmlabs.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val canvasView = CanvasView(this)
+        canvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        canvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(canvasView)
     }
 }
